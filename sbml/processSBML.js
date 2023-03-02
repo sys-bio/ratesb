b1f7470193e6ac5da923958be6e682c710931817
Aldrich Fan <longxf@uw.edu>
2023-03-01 20:50:19 -0800

    fix function definition

--- sbml/processSBML.js
+++ sbml/processSBML.js
@@ -6,26 +6,6 @@ class Reaction {
     }
 }
 
-class FunctionDefinition {
-    constructor( sbmlFunctionDefinition, libsbml) {
-        this.libsbml = libsbml;
-        this.sbmlFunctionDefinition = sbmlFunctionDefinition;
-        this.functionName = this.sbmlFunctionDefinition.getName();
-        this.id = this.sbmlFunctionDefinition.getId();
-        this.argumentNames = []
-        for (var i = 0; i < this.sbmlFunctionDefinition.getNumArguments(); i++) {
-            this.argumentNames.push(this.sbmlFunctionDefinition.getArgument(i).getName());
-        }
-        this.body = new libsbml.SBMLFormulaParser().formulaToL3String(this.sbmlFunctionDefinition.getBody());
-    }
-
-    toString() {
-        argumentCall = this.argumentNames.toString();
-        callStr = `${this.id}(${argumentCall})`;
-        return `${callStr}: ${this.body}`;
-    }
-}
-
 /** Process SBML file with an input SBML model in string */
 class ProcessSBML {
     constructor( modelStr, pyodide, libsbml ) {
