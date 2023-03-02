ae1992af8ac228e061455c8fd5f63b34e52976d9
Aldrich Fan <longxf@uw.edu>
2023-03-01 20:44:23 -0800

    function definitions

--- sbml/processSBML.js
+++ sbml/processSBML.js
@@ -6,6 +6,26 @@ class Reaction {
     }
 }
 
+class FunctionDefinition {
+    constructor( sbmlFunctionDefinition, libsbml) {
+        this.libsbml = libsbml;
+        this.sbmlFunctionDefinition = sbmlFunctionDefinition;
+        this.functionName = this.sbmlFunctionDefinition.getName();
+        this.id = this.sbmlFunctionDefinition.getId();
+        this.argumentNames = []
+        for (var i = 0; i < this.sbmlFunctionDefinition.getNumArguments(); i++) {
+            this.argumentNames.push(this.sbmlFunctionDefinition.getArgument(i).getName());
+        }
+        this.body = new libsbml.SBMLFormulaParser().formulaToL3String(this.sbmlFunctionDefinition.getBody());
+    }
+
+    toString() {
+        argumentCall = this.argumentNames.toString();
+        callStr = `${this.id}(${argumentCall})`;
+        return `${callStr}: ${this.body}`;
+    }
+}
+
 /** Process SBML file with an input SBML model in string */
 class ProcessSBML {
     constructor( modelStr, pyodide, libsbml ) {
@@ -18,12 +38,14 @@ class ProcessSBML {
         console.log(this.model);
         this.reactions = [];
         this.species = [];
+        this.parameters = [];
+        this.functionDefinitions = [];
         var i;
         for (i = 0; i < this.model.getNumSpecies(); i++) {
             this.species.push(this.model.getSpecies(i));
         }
         for (i = 0; i < this.model.getNumParameters(); i++) {
-            this.species.push(this.model.getParameter(i));
+            this.parameters.push(this.model.getParameter(i));
         }
         this.functionDefinitions = this.getFunctionDefinitions();
         console.log(this.functionDefinitions);
